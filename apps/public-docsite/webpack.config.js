af38e440d3380008c4c016dbcf2b5f1e90bfa1bf
David Zearing <dzearing@microsoft.com>
2021-02-10 12:34:54 -0800

    updating webpack config.

--- apps/public-docsite/webpack.config.js
+++ apps/public-docsite/webpack.config.js
@@ -1,62 +1,30 @@
-// @ts-check
+const webpack = require('webpack');
+const version = require('./package.json').version;
+const entryPointName = 'fabric-sitev5';
+const now = Date.now();
+const mode = 'production';
 
-module.exports = function(env, argv) {
-  const webpack = require('webpack');
-  const { remoteExternal } = require('@lading/webpack');
-  const resources = require('../../scripts/webpack/webpack-resources');
-  const getResolveAlias = require('../../scripts/webpack/getResolveAlias');
-  const { addMonacoWebpackConfig } = require('@fluentui/react-monaco-editor/scripts/addMonacoWebpackConfig');
-  // @ts-ignore
-  const version = require('./package.json').version;
-
-  const now = Date.now();
-
-  const entryPointName = 'fabric-sitev5';
-
-  return resources.createConfig(
-    entryPointName,
-    true,
-    addMonacoWebpackConfig({
-      entry: {
-        [entryPointName]: './lib/index.js',
-      },
-
-      output: {
-        chunkFilename: `${entryPointName}-${version}-[name]-${now}.js`,
-      },
-
-      plugins: [
-        new webpack.DefinePlugin({
-          process: JSON.stringify({
-            env: {
-              NODE_ENV: 'development',
-            },
-            argv: [],
-          }),
-        }),
-        new webpack.container.ModuleFederationPlugin({
-          remotes: {
-            '@fluentui/react': remoteExternal('fluentuiReact'),
-          },
-          shared: {
-            react: { singleton: true },
-            'react-dom': { singleton: true },
-          },
-        }),
-      ],
-
-      resolve: {
-        alias: {},
-      },
-
-      // The website config intentionally doesn't have React as an external because we bundle it
-      // to ensure we get a consistent version.
-
-      // resolve: {
-      //   alias: getResolveAlias(true /*useLib*/),
-      // },
+module.exports = {
+  mode,
+  cache: false,
+  entry: {
+    [entryPointName]: './lib/index.js',
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: '[name].js',
+    pathinfo: false,
+    globalObject: 'self',
+    chunkFilename: `${entryPointName}-${version}-[name]-${now}.js`,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      process: JSON.stringify({
+        env: {
+          NODE_ENV: mode,
+        },
+        argv: [],
+      }),
     }),
-    // always build the dev bundle too
-    /* only production */ false,
-  );
+  ],
 };
